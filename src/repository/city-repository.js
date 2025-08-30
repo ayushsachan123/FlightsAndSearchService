972e01e4c06d27728dd95cee6fbcee61b9b8c854
ayushsachan123 <ayushsachan.in@gmail.com>
2025-08-31 01:56:56 +0530

    completed city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,27 +1,48 @@
-const { City } = require('../models/index')
+const { City } = require("../models/index");
 
 class CityRepository {
-    async createCity({name}) {
-        try{
-           const city = await City.create({name});
-           return city;
-
-        }catch(error){
-           throw new Error("Error creating city");
-        }
+  async createCity({ name }) {
+    try {
+      const city = await City.create({ name });
+      return city;
+    } catch (error) {
+      throw new Error("Error creating city");
     }
+  }
 
-    async deleteCity(cityId) {
-        try{
-           await City.destroy({ 
-            where: { 
-                id: cityId 
-            } 
-        });
-        }catch(error){
-            throw new Error("Error deleting city");
-        }
+  async deleteCity(cityId) {
+    try {
+      await City.destroy({
+        where: {
+          id: cityId,
+        },
+      });
+    } catch (error) {
+      throw new Error("Error deleting city");
     }
+  }
+
+  async updateCity(cityId, data) {
+    try {
+      const city = await City.update(data, {
+        where: {
+          id: cityId,
+        },
+      });
+      return city;
+    } catch (error) {
+      throw new Error("Error updating city");
+    }
+  }
+
+  async getCity(cityId) {
+    try {
+      const city = await City.findByPk(cityId);
+      return city;
+    } catch (error) {
+      throw new Error("Error fetching city");
+    }
+  }
 }
 
 module.exports = CityRepository;
