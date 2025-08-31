f56b02c650615b96c3ef28f4a636fc78dbc6d421
ayushsachan123 <ayushsachan.in@gmail.com>
2025-08-31 15:09:54 +0530

    add city service

---
+++ src/services/city-service.js
@@ -0,0 +1,48 @@
+const { CityRepository } = require("../repository/index");
+
+class CityService {
+  constructor() {
+    this.CityRepository = new CityRepository();
+  }
+
+  async createCity(data) {
+    try {
+        const city = await this.CityRepository.create(data);
+        return city;
+    } catch (error) {
+        console.error("Error creating city:", error);
+        throw new Error("City creation failed");
+    }
+  }
+
+  async deleteCity(cityId) {
+    try {
+        await this.CityRepository.delete(cityId);
+    } catch (error) {
+        console.error("Error deleting city:", error);
+        throw new Error("City deletion failed");
+    }
+  }
+
+  async updateCity(cityId, data) {
+    try {
+        const updatedCity = await this.CityRepository.update(cityId, data);
+        return updatedCity;
+    } catch (error) {
+        console.error("Error updating city:", error);
+        throw new Error("City update failed");
+    }
+  }
+
+  async getCity(cityId) {
+    try {
+        const city = await this.CityRepository.getById(cityId);
+        return city;
+    } catch (error) {
+        console.error("Error getting city:", error);
+        throw new Error("City retrieval failed");
+    }
+  }
+}
+
+module.exports = CityService;
