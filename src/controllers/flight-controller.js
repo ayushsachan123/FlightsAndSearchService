dc93e7cbffa56bb7101433d4fa608bc182bde5cf
ayushsachan123 <ayushsachan.in@gmail.com>
2025-09-02 01:27:34 +0530

    Added flight-create request middleware

--- src/controllers/flight-controller.js
+++ src/controllers/flight-controller.js
@@ -4,7 +4,15 @@ const flightService = new FlightService();
 
 const create = async (req, res) => {
   try {
-    const flight = await flightService.createFlight(req.body);
+    const flight = await FlightService.createFlight({
+      flightNumber: req.body.flightNumber,
+      airplaneId: req.body.airplaneId,
+      departureAirportId: req.body.departureAirportId,
+      arrivalAirportId: req.body.arrivalAirportId,
+      arrivalTime: req.body.arrivalTime,
+      departureTime: req.body.departureTime,
+      price: req.body.price,
+    });
     return res.status(201).json({
       data: flight,
       success: true,
@@ -23,26 +31,26 @@ const create = async (req, res) => {
 };
 
 const getAll = async (req, res) => {
-    try {
-        const flights = await flightService.getAllFlights(req.query);
-        return res.status(200).json({
-            data: flights,
-            success: true,
-            message: "Flights fetched successfully",
-            err: {},
-        });
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            data: {},
-            success: false,
-            message: error.message,
-            err: error,
-        });
-    }
-}
+  try {
+    const flights = await flightService.getAllFlights(req.query);
+    return res.status(200).json({
+      data: flights,
+      success: true,
+      message: "Flights fetched successfully",
+      err: {},
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      data: {},
+      success: false,
+      message: error.message,
+      err: error,
+    });
+  }
+};
 
 module.exports = {
   create,
-  getAll
+  getAll,
 };
